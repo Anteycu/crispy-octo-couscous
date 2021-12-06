0b3c5b5ff0bd526117745213ed9a6a2815e62642
Nikolay Trunov <anteycu@gmail.com>
2021-12-06 12:29:08 +0200

    add component PaintingList and Section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import Painting from './components/Painting';
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
 import paintings from './paintings.json';
 
 const App = function () {
   return (
     <div>
-      {paintings.map(painting => (
-        <Painting
-          key={painting.id}
-          imageUrl={painting.url}
-          title={painting.title}
-          author={painting.author.tag}
-          profileUrl={painting.author.url}
-          price={painting.price}
-          quantity={painting.quantity}
-        />
-      ))}
+      <Section title="Top of week">
+        <PaintingList items={paintings} />
+      </Section>
+
+      <Section title="Best"/>
     </div>
   );
 };
